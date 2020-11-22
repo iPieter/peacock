907104ed860af3f9b7ec4a2ebaec4e1b8f3e649d
Pieter Delobelle <pieter.delobelle@gmail.com>
2020-11-22 16:36:20 +0100

    added robbert as link in resources

--- deploy_dtai.sh
+++ deploy_dtai.sh
@@ -7,5 +7,6 @@ pipenv run python main.py . -v -c --base "/~pieter.delobelle"
 scp -r ./build/ pieterd@ssh.cs.kuleuven.be:/cw/w3people/pieter.delobelle/
 
 ssh pieterd@ssh.cs.kuleuven.be rm -rf /cw/w3people/pieter.delobelle/public_html
-
 ssh pieterd@ssh.cs.kuleuven.be mv /cw/w3people/pieter.delobelle/build /cw/w3people/pieter.delobelle/public_html
+
+ssh pieterd@ssh.cs.kuleuven.be chmod +r /cw/w3people/pieter.delobelle/public_html/resources/DBRD_gender_test_v1.zip
---
+++ posts/2020/2020-09-24-ai-course/index.md
@@ -0,0 +1,6 @@
+# Session 1: Search representations
+- [The Towers of Hanoi »](https://www.youtube.com/watch?v=vNs1GmpUvq4)
+
+# Session 2: Informed search
+- [Path finding »]()
+- [Pancake flipping »]()
